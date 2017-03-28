8a3625e270badae66e0911ab31da0a262c84c161
GGOJIROCK <zhwan85@gmail.com>
2017-03-29 07:56:15 +0900

    인덱스 작성함
    인덱스 작성함

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('hello world')
+var express = require("express");
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host : '34.208.192.248:22'
+  user : ubuntu
+  database : 'testdb'
+});
+var app = express();
+connection.connect(function(err) {
+  if(!err) {
+    console.log("Database is connected ... \n\n");
+  } else {
+    console.log("Error connecting database ... \n\n");
+  }
+});
+app.get("/", function(request, response){
+
+});
+
+app.listen(3000);
