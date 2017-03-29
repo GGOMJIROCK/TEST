3c22182b043cb1ac7bff96f229e523b4e9e4941e
GGOJIROCK <zhwan85@gmail.com>
2017-03-30 08:15:21 +0900

    .
    .

--- index.js
+++ index.js
@@ -1,25 +1,45 @@
-var express = require('express');
-var mysql   = require('mysql');
-var dbconfig = require('./config/database.js');
-var connection = mysql.createConnection(dbconfig);
+// var express = require('express');
+// var mysql   = require('mysql');
+// var dbconfig = require('./config/database.js');
+// var connection = mysql.createConnection(dbconfig);
+//
+// var app = express();
+//
+// app.set('port', process.env.PORT || 3000);
+//
+// app.get('/', function(req, res) {
+//   res.send('Root');
+// });
+//
+// app.get('/testdb', function(req, res) {
+//
+//   connection.query('SELECT * from test_table', function(err, rows) {
+//     if(err) throw err;
+//     console.log('THE SOLUTION IS : ', rows);
+//     res.send(rows);
+//   });
+// });
+//
+// app.listen(app.get('port'), function () {
+//   console.log('Express server listening on port ' + app.get('port'));
+// });
 
-var app = express();
-
-app.set('port', process.env.PORT || 3000);
-
-app.get('/', function(req, res) {
-  res.send('Root');
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '12345678',
+  port     : 3306,
+  database : 'testdb'
 });
 
-app.get('/testdb', function(req, res) {
+connection.connect();
 
-  connection.query('SELECT * from test_table', function(err, rows) {
-    if(err) throw err;
-    console.log('THE SOLUTION IS : ', rows);
-    res.send(rows);
-  });
+connection.query('SELECT * from Persons', function(err, rows, fields) {
+  if (!err)
+    console.log('The solution is: ', rows);
+  else
+    console.log('Error while performing Query.', err);
 });
 
-app.listen(app.get('port'), function () {
-  console.log('Express server listening on port ' + app.get('port'));
-});
+connection.end();
