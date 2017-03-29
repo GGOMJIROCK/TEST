02f26232e7fcb067fb828111d90bbfcd93de5b96
GGOJIROCK <zhwan85@gmail.com>
2017-03-30 08:10:17 +0900

    .
    .

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '12345678',
-  port     : '3306',
-  database : 'testdb'
+var express = require('express');
+var mysql   = require('mysql');
+var dbconfig = require('./config/database.js');
+var connection = mysql.createConnection(dbconfig);
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', function(req, res) {
+  res.send('Root');
 });
 
-connection.connect();
+app.get('/testdb', function(req, res) {
 
-connection.query('SELECT * from test_table', function(err, rows, fields) {
-  if (!err)
-    console.log('The solution is: ', rows);
-  else
-    console.log('Error while performing Query.', err);
+  connection.query('SELECT * from test_table', function(err, rows) {
+    if(err) throw err;
+    console.log('THE SOLUTION IS : ', rows);
+    res.send(rows);
+  });
 });
 
-connection.end();
+app.listion(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
