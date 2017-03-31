ecfd99e37ba6408c4801c4da0d27869f8bcdc476
GGOJIROCK <zhwan85@gmail.com>
2017-03-31 21:25:43 +0900

    .

--- index.js
+++ index.js
@@ -7,27 +7,40 @@
 //         console.log('Connect 80 port');
 // });
 
-var express = require('express');
-var mysql   = require('mysql');
-var dbconfig = require('./config/database.js');
-var connection = mysql.createConnection(dbconfig);
-
-var app = express();
-
-app.set('port', 80);//process.env.PORT || 3000);
-
-app.get('/', function(req, res) {
-  connection.connect();
-  connection.query('call sample()', function(err, rows, fields) {
-    if (!err)
-      res.send('The solution is: ' + rows);
-    else
-      console.log('Error while performing Query.', err);
-  });
-  res.send('Root');
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '12345678',
+  port     : '3306',
+  database : 'testdb'
 });
 
+connection.connect();
 
+connection.query('SELECT * from test_table', function(err, rows, fields) {
+  if (!err)
+    console.log('The solution is: ', rows);
+  else
+    console.log('Error while performing Query.', err);
+});
+
+connection.end();
+
+
+// var express = require('express');
+// var mysql   = require('mysql');
+// var dbconfig = require('./config/database.js');
+// var connection = mysql.createConnection(dbconfig);
+//
+// var app = express();
+//
+// app.set('port', 80);//process.env.PORT || 3000);
+//
+// app.get('/', function(req, res) {
+//   res.send('Root');
+// });
+//
 // app.get('/testdb', function(req, res) {
 //
 //   connection.query('call sample()', function(err, rows) {
@@ -36,10 +49,10 @@ app.get('/', function(req, res) {
 //     res.send(rows);
 //   });
 // });
-
-app.listen(app.get('port'), function () {
-  console.log('Express server listening on port ' + app.get('port'));
-});
+//
+// app.listen(app.get('port'), function () {
+//   console.log('Express server listening on port ' + app.get('port'));
+// });
 
 // var mysql      = require('mysql');
 // var connection = mysql.createConnection({
